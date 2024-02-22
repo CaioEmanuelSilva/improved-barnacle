161d4d3f668e834d4f416d4a627b1978a1d6cbc7
CaioEmanuelSilva <140855083+CaioEmanuelSilva@users.noreply.github.com>
2024-02-22 08:44:50 +0000

    Atualizar informações de contato do site

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Caio Silva",
+  title: "Data analyst",
+  email: "caio.emnl.silva@gmail.com",
+  gitHub: "CaioEmanuelSilva",
+  instagram: "scaioemanuel",
+  linkedIn: "caio-emanuel-silva",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "caio_silva00702",
+  youTube: "@Skyatteck00702",
 };
 
 const primaryColor = "#4E567E";
